a78b6efdde9e38e4df25282576901612de71e563
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-12-09 14:41:55 +0100

    show_image

--- gallery/views.py
+++ gallery/views.py
@@ -10,7 +10,8 @@ def home(request):
     return render(request, 'gallery/home.html', context)
 
 def show_image(request, pk):
-    return render(request, 'gallery/show_image.html')
+    image = Image.objects.get(id=pk)
+    return render(request, 'gallery/show_image.html', {'image': image })
 
 def add_image(request):
     return render(request, 'gallery/add_image.html')
