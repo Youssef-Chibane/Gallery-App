d93ee063a4fe4b746f0eff311b4c6a894b9f9fa9
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-12-09 18:39:24 +0100

    add-image&filter-category

--- gallery/views.py
+++ gallery/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Image
 # Create your views here.
 
 
 def home(request):
+
+    category = request.GET.get('category')
+    if category == None:
+        images = Image.objects.all()
+    else:
+        images = Image.objects.filter(category__name=category)
+
     categories = Category.objects.all()
-    images = Image.objects.all()
     context = {'categories': categories, 'images': images}
     return render(request, 'gallery/home.html', context)
 
@@ -14,4 +20,28 @@ def show_image(request, pk):
     return render(request, 'gallery/show_image.html', {'image': image })
 
 def add_image(request):
-    return render(request, 'gallery/add_image.html')
+    categories = Category.objects.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        images = request.FILES.getlist('images')
+
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(
+                name=data['category_new'])
+        else:
+            category = None
+
+        for image in images:
+            image = Image.objects.create(
+                category=category,
+                description=data['description'],
+                image=image,
+            )
+
+        return redirect('Home')
+
+    context = {'categories': categories}
+    return render(request, 'gallery/add_image.html', context)
